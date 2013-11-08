5920dad984cee1bb429ac97544988d94ad7879da
Nikolas Poklitar <nikolas.poklitar@gmail.com>
2013-11-07 23:14:01 -0600

    lots of boiler plate ideas updates. and setting up requirejs :):)

---
+++ src/modules/GamePlay.js
@@ -0,0 +1,34 @@
+function GamePlay(){
+	map: null,
+
+}
+
+function initGamePlay(){
+	var gp = new GamePlay();
+	gp.map = new Map();
+	
+    return gp;
+}
+
+
+GamePlay.prototype.update = function update(){
+
+
+
+
+
+
+
+};
+
+
+
+GamePlay.prototype.draw = function update(){
+
+
+
+
+
+
+
+};
